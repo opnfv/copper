09ad21ca28f9ab7a2722a5e0aab5a83fd022c4d3
blsaws <blsaws@hotmail.com>
2015-11-11 09:50:21 -0800

    Move conf.py to /etc
    Change-Id: Idcdf8bba4aed2de9380d8441816d26152dad52e1
    Signed-off-by: blsaws <blsaws@hotmail.com>

--- etc/conf.py
+++ etc/conf.py
@@ -17,8 +17,9 @@ source_suffix = '.rst'
 master_doc = 'index'
 pygments_style = 'sphinx'
 html_use_index = False
+html_theme = 'sphinx_rtd_theme'
 
-pdf_documents = [('index', u'Copper', u'Copper Project', u'OPNFV')]
+pdf_documents = [('index', u'OPNFV', u'OPNFV Project', u'OPNFV')]
 pdf_fit_mode = "shrink"
 pdf_stylesheets = ['sphinx','kerning','a4']
 #latex_domain_indices = False
@@ -28,7 +29,7 @@ latex_elements = {
     'printindex': '',
 }
 
-project = u'Copper: Virtual Infrastructure Deployment Policies'
+project = u'OPNFV: Template documentation config'
 copyright = u'%s, OPNFV' % datetime.date.today().year
 version = u'1.0.0'
 release = u'1.0.0'
