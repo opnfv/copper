1074117e1c3e7fc6a44f00f9389fec47ad70150f
blsaws <blsaws@hotmail.com>
2016-01-25 19:19:55 -0800

    Include license info in all files
    JIRA: COPPER-13
    
    Change-Id: Ia3b19854a13d593cc15e51b3460598e61bf66ab0
    Signed-off-by: blsaws <blsaws@hotmail.com>

--- tests/adhoc/smoke01-clean.sh
+++ tests/adhoc/smoke01-clean.sh
@@ -1,4 +1,29 @@
 #!/bin/bash
+# Copyright 2015 Open Platform for NFV Project, Inc. and its contributors
+#  
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#  
+# http://www.apache.org/licenses/LICENSE-2.0
+#  
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# What this is: Cleanup script for a basic test to validate an OPNFV install. 
+#
+# Status: this is a work in progress, under test. 
+#
+# How to use:
+#   Install test server per https://wiki.opnfv.org/copper/academy/congress/test
+#   $ source ~/git/copper/tests/adhoc/smoke01.sh
+#   After test, cleanup with
+#   $ source ~/git/copper/tests/adhoc/smoke01-clean.sh
+
 set -x #echo on
 
 instance=$(nova list | awk "/ cirros1 / { print \$2 }")
