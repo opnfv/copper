4d5091d11ce7088cfe3c425cc2d4be2157dbd31e
ubuntu <ubuntu@jumphost.opnfv.org>
2016-02-09 18:35:24 -0800

    Update congress bash install scripts for more automation.
    JIRA: COPPER-2
    
    Change-Id: I4b34f83c7c7c939934e8badd3e24eb926f07397b
    Signed-off-by: ubuntu <ubuntu@jumphost.opnfv.org>

---
+++ components/congress/joid/install_congress_2.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Copyright 2015-2016 Open Platform for NFV Project, Inc. and its contributors
+#  
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#  
+# http://www.apache.org/licenses/LICENSE-2.0
+#  
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# This is script 2 of 5 for installation of Congress on an Ubuntu 14.04 
+# LXC container in the OPNFV Controller node.
+# Prequisite: OPFNV install per https://wiki.opnfv.org/copper/academy/joid
+#
+# On jumphost:
+# Download admin-openrc.sh from Horizon and save in ~
+# source ~/git/copper/components/congress/joid/install_congress_1.sh
+# (copies install_congress_2.sh to node1-control and executes it)
+
+sudo lxc-clone -o juju-trusty-lxc-template -n juju-trusty-congress
+sudo lxc-start -n juju-trusty-congress -d
+sleep 10
+sudo lxc-info -n juju-trusty-congress
