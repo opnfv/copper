072d4cfc11121c3ac3183541da70f3ce68e846cf
blsaws <bryan.sullivan@att.com>
2016-03-14 14:09:43 -0700

    Split DMZ test into policy creation and VM etc creation.
    JIRA: COPPER-4
    
    Change-Id: I8acaeab280f6fe5e794fdb5cf3e6651a2042c5a7
    Signed-off-by: blsaws <bryan.sullivan@att.com>

--- tests/adhoc/dmz01_1.sh
+++ tests/adhoc/dmz01_1.sh
@@ -27,7 +27,10 @@
 #
 # How to use:
 #   Install test server per https://wiki.opnfv.org/copper/academy/congress/test
-#   $ source ~/git/copper/tests/adhoc/dmz01.sh
+#   # Create Congress policy
+#   $ source ~/git/copper/tests/adhoc/dmz01_1.sh
+#   # Create VMs etc that exercise policy
+#   $ source ~/git/copper/tests/adhoc/dmz01_2.sh
 #   After test, cleanup with (not yet implemented)
 #   $ source ~/git/copper/tests/adhoc/dmz01-clean.sh
 
@@ -37,51 +40,12 @@ source ~/admin-openrc.sh <<EOF
 openstack
 EOF
 
-image=$(openstack image list | awk "/ cirros-0.3.3-x86_64 / { print \$2 }")
-if [ "$image" == "" ]; then glance --os-image-api-version 1 image-create --name cirros-0.3.3-x86_64 --disk-format qcow2 --location http://download.cirros-cloud.net/0.3.3/cirros-0.3.3-x86_64-disk.img --container-format bare
-fi
-
-image=$(openstack image list | awk "/ cirros-0.3.3-x86_64-dmz / { print \$2 }")
-if [ "$image" == "" ]; then glance --os-image-api-version 1 image-create --name cirros-0.3.3-x86_64-dmz --disk-format qcow2 --location http://download.cirros-cloud.net/0.3.3/cirros-0.3.3-x86_64-disk.img --container-format bare
-fi
-
-IMAGE_ID=$(glance image-list | awk "/ cirros-0.3.3-x86_64-dmz / { print \$2 }")
-
-glance --os-image-api-version 2 image-tag-update $IMAGE_ID "dmz"
-
-neutron net-create test_public --router:external=true --provider:network_type=flat --provider:physical_network=physnet1
-
-neutron subnet-create --disable-dhcp test_public 192.168.10.0/24
-
-neutron net-create test_internal
-
-neutron subnet-create test_internal 10.0.0.0/24 --name test_internal --gateway 10.0.0.1 --enable-dhcp --allocation-pool start=10.0.0.2,end=10.0.0.254 --dns-nameserver 8.8.8.8
-
-neutron router-create test_router
-
-neutron router-gateway-set test_router test_public
-
-neutron router-interface-add test_router subnet=test_internal
-# add a delay since the previous command takes the neutron-api offline for a while (?)
-sleep 30
-
-test_internal_NET=$(neutron net-list | awk "/ test_internal / { print \$2 }")
-
-neutron security-group-create dmz
-
-neutron security-group-rule-create --direction ingress  dmz
-
-nova boot --flavor m1.tiny --image cirros-0.3.3-x86_64 --nic net-id=$test_internal_NET --security-groups dmz cirros1
-
-nova boot --flavor m1.tiny --image cirros-0.3.3-x86_64-dmz --nic net-id=$test_internal_NET  --security-groups dmz cirros2
-
 openstack congress policy create test
 
 openstack congress policy rule create test "dmz_server(x) :- nova:servers(id=x,status='ACTIVE'), neutronv2:ports(id, device_id, status='ACTIVE'),  neutronv2:security_group_port_bindings(pid, sg), neutronv2:security_groups(sg,name='dmz')" --name dmz_server
 
 # currently failing "Rule already exists::An unknown exception occurred. (HTTP 409)..."
-openstack congress policy rule create test "dmz_placement_error(id) :- nova:servers(
-id,name,hostId,status,tenant_id,user_id,image,flavor,OS1,OS2), not glance:images(image,tags='dmz'), dmz_server(x)" --name dmz_placement_error
+# openstack congress policy rule create test "dmz_placement_error(id) :- nova:servers(id,name,hostId,status,tenant_id,user_id,image,flavor,OS1,OS2), not glance:images(image,tags='dmz'), dmz_server(x)" --name dmz_placement_error
 
 # validated rules created during test development
 # openstack congress policy rule create test "active_servers(x) :- nova:servers(id=x, status='ACTIVE')" --name active_servers
