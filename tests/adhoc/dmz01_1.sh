c1b5d333ee859cc1f4463a7a788a64d2877a9e6f
blsaws <bryan.sullivan@att.com>
2016-03-14 17:04:32 -0700

    Testing additional policies
    JIRA: COPPER-4
    
    Change-Id: Ice3323d2bc6490ececb519a067224720aa7ddf32
    Signed-off-by: blsaws <bryan.sullivan@att.com>

--- tests/adhoc/dmz01_1.sh
+++ tests/adhoc/dmz01_1.sh
@@ -44,8 +44,10 @@ openstack congress policy create test
 
 openstack congress policy rule create test "dmz_server(x) :- nova:servers(id=x,status='ACTIVE'), neutronv2:ports(id, device_id, status='ACTIVE'),  neutronv2:security_group_port_bindings(pid, sg), neutronv2:security_groups(sg,name='dmz')" --name dmz_server
 
+openstack congress policy rule create test "cirros(x) :- nova:servers(id=x,image,status='ACTIVE'), glance:images(image,name='cirros')" --name cirros_server
+
 # currently failing "Rule already exists::An unknown exception occurred. (HTTP 409)..."
-# openstack congress policy rule create test "dmz_placement_error(id) :- nova:servers(id,name,hostId,status,tenant_id,user_id,image,flavor,OS1,OS2), not glance:images(image,tags='dmz'), dmz_server(x)" --name dmz_placement_error
+# openstack congress policy rule create test "dmz_placement_error(id) :- nova:servers(id,name,hostId,status,tenant_id,user_id,image,flavor,OS1,OS2), not glance:images(image,status,gname,container_format,created_at,updated,disk_format,owner,protected,min_ram,min_disk,checksum,size,file,kernel_id,ramdisk_id,ramdisk_id,schema,visibility,tags='dmz'), dmz_server(x)" --name dmz_placement_error
 
 # validated rules created during test development
 # openstack congress policy rule create test "active_servers(x) :- nova:servers(id=x, status='ACTIVE')" --name active_servers
