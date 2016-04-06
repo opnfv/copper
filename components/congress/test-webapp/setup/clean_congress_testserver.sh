6df5ce72967055017d69bed82656507f8748f0b4
blsaws <bryan.sullivan@att.com>
2016-04-06 11:20:37 -0700

    Add command echo with debug parameter control.
    JIRA: COPPER-4
    
    Change-Id: I9937fdb41748165a98aafe3b5f35771561ff3f53
    Signed-off-by: blsaws <bryan.sullivan@att.com>

--- components/congress/test-webapp/setup/clean_congress_testserver.sh
+++ components/congress/test-webapp/setup/clean_congress_testserver.sh
@@ -13,17 +13,25 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
 # This is a cleanup script for installation of the Congress testserver
 # on an Ubuntu 14.04 LXC container in the jumphost.
 # Presumably something has failed, and any record of the testserver
 # needs to be removed, so you can try the install again.
+# On jumphost:
+# source ~/clean_congress_testserver.sh <debug>
+# <debug> indicates whether to turn on command echoing
 
 source ~/admin-openrc.sh <<EOF
 openstack
 EOF
+
 source ~/env.sh
-set -x
+
+if [ $# -gt 0 ]; then
+  if [ $1 == "debug" ]; then set -x #echo on
+  fi
+fi
+
 sudo lxc-stop --name trusty-copper
 sudo lxc-destroy --name trusty-copper
 rm -rf ~/coppertest
