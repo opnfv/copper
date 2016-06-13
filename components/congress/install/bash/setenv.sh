caa5fee4411ba99cee8f662ce81debe1dc514075
blsaws <bryan.sullivan@att.com>
2016-06-13 07:17:42 -0700

    Break out environment setup script.
    JIRA: COPPER-4
    
    Change-Id: Ic0e0244c5a9ee1f509ae8b1937bb946b68e1e957
    Signed-off-by: blsaws <bryan.sullivan@att.com>

---
+++ components/congress/install/bash/setenv.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+# Copyright 2015-2016 AT&T Intellectual Property, Inc
+#  
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#  
+# http://www.apache.org/licenses/LICENSE-2.0
+#  
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# This script sets up the OpenStack CLI environment variables.
+# Prerequisites: 
+# - OPFNV installed via JOID or Apex
+# - For Apex installs, on the jumphost, ssh to the undercloud VM and
+#     $ su stack
+# $ cd ~
+# $ wget https://git.opnfv.org/cgit/copper/plain/components/congress/install/bash/setenv.sh
+# $ bash setenv.sh
+
+dist=`grep DISTRIB_ID /etc/*-release | awk -F '=' '{print $2}'`
+
+if [ "$dist" == "Ubuntu" ]; then
+  # Ubuntu
+  echo "Ubuntu-based install"
+  echo "Create the environment file"
+  KEYSTONE_HOST=$(juju status --format=short | awk "/keystone\/0/ { print \$3 }")
+  cat <<EOF >~/env.sh
+export CONGRESS_HOST=$(juju status --format=short | awk "/openstack-dashboard/ { print \$3 }")
+export HORIZON_HOST=$(juju status --format=short | awk "/openstack-dashboard/ { print \$3 }")
+export KEYSTONE_HOST=$KEYSTONE_HOST
+export CEILOMETER_HOST=$(juju status --format=short | awk "/ceilometer\/0/ { print \$3 }")
+export CINDER_HOST=$(juju status --format=short | awk "/cinder\/0/ { print \$3 }")
+export GLANCE_HOST=$(juju status --format=short | awk "/glance\/0/ { print \$3 }")
+export NEUTRON_HOST=$(juju status --format=short | awk "/neutron-api\/0/ { print \$3 }")
+export NOVA_HOST=$(juju status --format=short | awk "/nova-cloud-controller\/0/ { print \$3 }")
+export OS_USERNAME=admin
+export OS_PASSWORD=openstack
+export OS_TENANT_NAME=admin
+export OS_AUTH_URL=http://$KEYSTONE_HOST:5000/v2.0
+export OS_REGION_NAME=Canonical
+EOF
+else
+  # Centos
+  echo "Centos-based install"
+  echo "Setup undercloud environment so we can get overcloud Controller server address"
+  source ~/stackrc
+  echo "Get address of Controller node"
+  export CONTROLLER_HOST1=$(openstack server list | awk "/overcloud-controller-0/ { print \$8 }" | sed 's/ctlplane=//g')
+  echo "Create the environment file"
+  cat <<EOF >~/env.sh
+export CONGRESS_HOST=$CONTROLLER_HOST1
+export KEYSTONE_HOST=$CONTROLLER_HOST1
+export CEILOMETER_HOST=$CONTROLLER_HOST1
+export CINDER_HOST=$CONTROLLER_HOST1
+export GLANCE_HOST=$CONTROLLER_HOST1
+export NEUTRON_HOST=$CONTROLLER_HOST1
+export NOVA_HOST=$CONTROLLER_HOST1
+EOF
+  cat ~/overcloudrc >>~/env.sh
+  source ~/overcloudrc
+  export OS_REGION_NAME=$(openstack endpoint list | awk "/ nova / { print \$4 }")
+  # sed command below is a workaound for a bug - region shows up twice for some reason
+  cat <<EOF | sed '$d' >>~/env.sh
+export OS_REGION_NAME=$OS_REGION_NAME
+EOF
+fi
+source ~/env.sh
+
